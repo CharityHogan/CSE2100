4cdde5347430ce52da06c43d38c11bb2e558b791
codingplaner <charityhogan@byui.edu>
2025-09-20 15:30:57 +0200

    prep3 complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Choose a number between 1 and 100.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string answer = Console.ReadLine();
+            guess = int.Parse(answer);
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+
+
+
+
     }
 }
