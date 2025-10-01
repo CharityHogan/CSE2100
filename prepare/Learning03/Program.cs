0766eca17b70429a0ed7771ba3b42d0594613fef
codingplaner <charityhogan@byui.edu>
2025-10-01 17:59:26 +0200

    complete

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,3 +1,5 @@
+using System;
+
 public class Fraction
 {
     // Private attributes (encapsulation)
@@ -53,5 +55,36 @@ public class Fraction
     public double GetDecimalValue()
     {
         return (double)_top / _bottom;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Using different constructors
+        Fraction f1 = new Fraction();         // 1/1
+        Fraction f2 = new Fraction(5);        // 5/1
+        Fraction f3 = new Fraction(3, 4);     // 3/4
+        Fraction f4 = new Fraction(1, 3);     // 1/3
+
+        // Display results
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        // Try using setters
+        f4.SetTop(2);
+        f4.SetBottom(5);
+        Console.WriteLine(f4.GetFractionString()); // 2/5
+        Console.WriteLine(f4.GetDecimalValue());   // 0.4
     }
 }
