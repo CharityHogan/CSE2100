8e052309c47b764ca5bbbc06cef7d05b9d3e4b33
codingplaner <charityhogan@byui.edu>
2025-10-04 14:50:07 +0200

    bugs are finally fixed

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,173 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        List<Scripture> scriptures = new List<Scripture>()
+        {
+            new Scripture(new Reference("Proverbs", 3, 5, 6),
+                "Trust in the Lord with all thine heart; and lean not unto thine own understanding."),
+            new Scripture(new Reference("John", 3, 16),
+                "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life."),
+            new Scripture(new Reference("Philippians", 4, 13),
+                "I can do all things through Christ which strengtheneth me.")
+        };
+
+        Random random = new Random();
+        Scripture scripture = scriptures[random.Next(scriptures.Count)];
+
+        while (true)
+        {
+            SafeClear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("\nPress Enter to hide more words or type 'quit' to end:");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+                break;
+
+            scripture.HideRandomWords(3, random);
+
+            if (scripture.IsCompletelyHidden())
+            {
+                SafeClear();
+                Console.WriteLine(scripture.GetDisplayText());
+                Console.WriteLine("\nAll words are hidden. Great job memorizing!");
+                break;
+            }
+        }
+    }
+
+    static void SafeClear()
+    {
+        try
+        {
+            Console.Clear();
+        }
+        catch
+        {
+            for (int i = 0; i < 30; i++)
+                Console.WriteLine();
+        }
+    }
+}
+
+public class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _startVerse;
+    private int _endVerse;
+
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _startVerse = verse;
+        _endVerse = verse;
+    }
+
+    public Reference(string book, int chapter, int startVerse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _startVerse = startVerse;
+        _endVerse = endVerse;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_startVerse == _endVerse)
+            return $"{_book} {_chapter}:{_startVerse}";
+        else
+            return $"{_book} {_chapter}:{_startVerse}-{_endVerse}";
+    }
+}
+
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_isHidden)
+            return new string('_', _text.Length);
+        else
+            return _text;
+    }
+}
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] splitText = text.Split(' ');
+        foreach (string word in splitText)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide, Random random)
+    {
+        int availableWords = _words.Count(w => !w.IsHidden());
+        numberToHide = Math.Min(numberToHide, availableWords);
+        
+        int count = 0;
+        while (count < numberToHide)
+        {
+            int index = random.Next(_words.Count);
+            if (!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+                count++;
+            }
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        List<string> displayWords = new List<string>();
+        foreach (Word word in _words)
+        {
+            displayWords.Add(word.GetDisplayText());
+        }
+        return $"{_reference.GetDisplayText()} - {string.Join(" ", displayWords)}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+                return false;
+        }
+        return true;
     }
 }
