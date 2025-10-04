5b7776a043b199120a77ca05cbc164acbc21bd24
codingplaner <charityhogan@byui.edu>
2025-10-04 15:24:36 +0200

    adding book, chapters, start, and end verse together

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -12,4 +12,20 @@ public class Reference
         _startVerse = vesre;
         _endVerse = vesre;
     }
+
+    public Reference(string book, int chapter, int startVerse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _startVerse = startVerse;
+        _endVerse = endVerse;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_startVerse == _endVerse)
+            return _book + " " + _chapter + ":" + _startVerse;
+        else
+            return _book + " " + _chapter + ":" + _startVerse + "-" + _endVerse;
+    }
 }
