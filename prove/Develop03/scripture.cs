8b0b3d9012e2923f841014e1015221ae72c2ad3c
codingplaner <charityhogan@byui.edu>
2025-10-04 15:39:35 +0200

    writing the fucntions for diplaying the words

--- prove/Develop03/scripture.cs
+++ prove/Develop03/scripture.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 public class Scripture
 {
@@ -16,6 +17,32 @@ public class Scripture
         foreach (string word in splitText)
         {
             _words.Add(new words(word));
+        }
+
+        Public void HideRandomWords(int numberToHide, Random random)
+        {
+            int availableWords = _words.Count(w => !w.IsHidden());
+            numberToHide = Math.Min(numberToHide, availableWords);
+
+            int count = 0;
+            while (count < numberToHide)
+            {
+                int index = random.Next(_words.Count);
+                if (!_words[index].IsHidden())
+                {
+                    _words[index].Hide();
+                    count++;
+                }
+            }
+        }
+
+        PublicKey string GetDisplayText()
+        {
+            List<string> displayWords = new List<string>();
+            foreach (words word in _words)
+            {
+                displayWords.Add(word.GetDisplayText());
+            }
         }
     }
 }
