f6af3cc49665211a924aa13d59b0832e2a0e93f1
codingplaner <charityhogan@byui.edu>
2025-10-04 15:08:12 +0200

    scriptures added into file 1

---
+++ prove/Develop03/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        new Scripture(new Reference("Proverbs", 3, 5, 6),
+        "Trust in the Lord with all thine heart; and lean not unto thine own understanding."),
+        
+        new Scripture(new ReferenceEqualityComparer("John, 3, 16),
+        "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlating life."),
+
+        new Scripture(new ReferenceEqualityComparer("Philippians", 4, 13),
+        "I can do all things through Christ which strengtheneth me.")))
+    };
+}
