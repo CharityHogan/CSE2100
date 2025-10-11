1c68a5921b4f5a48e21ea4cfdea1f6f367b7f0a3
codingplaner <charityhogan@byui.edu>
2025-10-11 17:06:00 +0200

    LISTS!!!

--- prove/Develop04/Listing_activity.cs
+++ prove/Develop04/Listing_activity.cs
@@ -1,4 +1,33 @@
 class ListingActivity : Activity
 {
     public void DoActivity()
+    {
+        Start();
+        string[] prompts =
+        {
+            "List people who you appreciate.",
+            "List personal strengths you have.",
+            "List things you are grateful for.",
+            "List your favorite hobbies.",
+            "List places you want to visit."
+        };
+
+        Random rnd = new Random();
+        Console.WriteLine(prompts[rnd.Next(prompts.Length)]);
+        Pause(3);
+
+        int elapsed = 0;
+        int count = 0;
+
+        while (elapsed < Duration)
+        {
+            Console.Write("Enter item: ");
+            string input = Console.ReadLine();
+            count++;
+            elapsed += 2;
+        }
+
+        Console.WriteLine("You listed " + count + " items!");
+        End();
+    }
 }
