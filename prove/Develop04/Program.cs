3246b5bbd171c97c8a522851368ce9a008195a5c
codingplaner <charityhogan@byui.edu>
2025-10-11 16:41:23 +0200

    writing for the beginning of the program

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,3 +1,19 @@
 using System;
 using System.Threading;
 
+class Activity
+{
+    public string Name;
+    public string Description;
+    public int Duration;
+
+    public void Start()
+    {
+        Console.Clear();
+        Console.WriteLine("Starting " + Name + " Activity!");
+        Console.WriteLine(Description);
+        Console.WriteLine("Duration: " + Duration + " seconds");
+        Console.Write("Get ready...");
+        Pause(3);
+    }
+}
