aa5c9be1c12eb41aa750c913d25ae1553856dd3c
codingplaner <charityhogan@byui.edu>
2025-10-15 22:54:19 +0200

    loop!

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -8,6 +8,15 @@ class Program
         List<shape> shapes = new List<shape>();
 
 
-        
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 3, 5));
+        shapes.Add(new Circle("Green", 2.5));
+
+        foreach (shape shape in shapes)
+        {
+            Console.WriteLine("Shape color: " + shape.GetColor());
+            Console.WriteLine("Shape area: " + shape.GetArea());
+            Console.WriteLine();
+        }
     }
 }
