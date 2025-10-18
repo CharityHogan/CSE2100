99ddaff4f261e44ef16ae739aab25a699fac727c
codingplaner <charityhogan@byui.edu>
2025-10-18 13:58:43 +0200

    final plan!

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -7,3 +7,15 @@ class Program
         Console.WriteLine("Hello Foundation4 World!");
     }
 }
+
+// Program 1: Abstraciton with Youtube videos  
+//create a video class to store video info and a list of comments, then display each video's details and comments.
+
+// Program 2: Encapsulation with online ordering
+// Create classes for Product, Customer, Address, and Order to calculate total costs and print packing and shipping labels
+//
+// Program 3: Inheritance with Event Planning
+// Create a base event class and derived classes for lectures, receptions, andoutdoor events, then display different event descriptions
+
+// Program 4: Polymorphism with Exercise Tracking
+// Create a base activity class adn derived classes for running, cycling, and swimming, then calculate and isplay distance, speed, pace, and a summary for each activity
