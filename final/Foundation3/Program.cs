897bbfaf318c64b52d9be174620a9922c18bf0bb
codingplaner <charityhogan@byui.edu>
2025-10-28 17:06:47 +0100

    reception and outdoorgathering created

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -5,9 +5,11 @@ class Program
 {
     static void Main()
     {
-        Address address1 = new Address("100 Main St", "New Yourk", "NY", "USA");
+        address address1 = new Address("100 Main St", "New Yourk", "NY", "USA");
 
-        Lecture lecture = new Lecture("C# Basics", "Learn the basics of C#", "Nov 10", "10:00 AM", address1, "Dr. Smith", 50);
+        lecture lecture = new Lecture("C# Basics", "Learn the basics of C#", "Nov 10", "10:00 AM", address1, "Dr. Smith", 50);
+        reception reception = new RankException("Tech Meetup", "Networking for developers", "Dec 5", "6:00 PM", address1, "rsvp@techmeet.com");
+        outDoorGathering outdoor = new outDoorGathering("Picnic Day", "Fun outdoor games", "May 15", "12:00 PM", address1, "Sunny");
         
     }
 }
