ae30336669c5b4e4d0f89ee66ac02e9c2241f1fb
codingplaner <charityhogan@byui.edu>
2025-10-28 17:22:18 +0100

    working on Address class

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -5,11 +5,27 @@ class Program
 {
     static void Main()
     {
-        address address1 = new Address("100 Main St", "New Yourk", "NY", "USA");
+        Address address1 = new Address("100 Main St", "New Yourk", "NY", "USA");
 
-        lecture lecture = new Lecture("C# Basics", "Learn the basics of C#", "Nov 10", "10:00 AM", address1, "Dr. Smith", 50);
-        reception reception = new RankException("Tech Meetup", "Networking for developers", "Dec 5", "6:00 PM", address1, "rsvp@techmeet.com");
-        outDoorGathering outdoor = new outDoorGathering("Picnic Day", "Fun outdoor games", "May 15", "12:00 PM", address1, "Sunny");
-        
+        Lecture lecture = new Lecture("C# Basics", "Learn the basics of C#", "Nov 10", "10:00 AM", address1, "Dr. Smith", 50);
+        Reception reception = new RankException("Tech Meetup", "Networking for developers", "Dec 5", "6:00 PM", address1, "rsvp@techmeet.com");
+        OutdoorGathering outdoor = new outDoorGathering("Picnic Day", "Fun outdoor games", "May 15", "12:00 PM", address1, "Sunny");
+
+        Event[] events = { lecture, reception, outdoor };
+
+        foreach (Event e in events)
+        {
+            Console.WriteLine(e.StandardDetails());
+            Console.WriteLine();
+            Console.WriteLine(e.FullDetails());
+            Console.WriteLine();
+            Console.WriteLine(e.ShortDescription());
+            Console.WriteLine("---------------------------");
+        }
+
+        Console.ReadLine();
     }
 }
+
+
+class Address
