69c4fc8dfa33c6f29ef8d2bdd8d79810cdb48133
codingplaner <charityhogan@byui.edu>
2025-10-28 17:01:16 +0100

    working on making address  for program 3

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Net.Sockets;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Address address1 = new Address("100 Main St", "New Yourk", "NY", "USA");
+
+        Lecture lecture = new Lecture("C# Basics", "Learn the basics of C#", "Nov 10", "10:00 AM", address1, "Dr. Smith", 50);
+        
     }
 }
